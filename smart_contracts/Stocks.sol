b842297c2526809aab00ec78cd162e4190ccba93
artemgur <artemgur01@gmail.com>
2022-12-11 20:45:22 +0300

    Progress

--- smart_contracts/Stocks.sol
+++ smart_contracts/Stocks.sol
@@ -4,12 +4,23 @@ pragma solidity >=0.7.0 <0.9.0;
 
 
 contract Stocks {
-    address public director;
-    int32 public stocksCount;
 
-    constructor(int32 _stocksCount) {
+    struct Stockholder {
+        uint32 stocksCount;
+        uint256 sellPrice;
+        uint32 sellCount;
+    }
+
+
+    address public director;
+    uint32 public stocksCount;
+
+    mapping(address => Stockholder) public stockholders;
+
+    constructor(uint32 _stocksCount) {
         director = msg.sender;
         stocksCount = _stocksCount;
+        stockholders[address(this)].stocksCount = _stocksCount;
     }
 
     // modifier to check if caller is owner
@@ -23,8 +34,23 @@ contract Stocks {
         _;
     }
 
+    modifier isStockholder(address addr) {
+        require(stockholders[addr].stocksCount != 0, "Caller is not stockholder");
+        _;
+    }
+
     modifier notLargerThanBalance(uint256 amount) {
         require(address(this).balance >= amount);
+        _;
+    }
+
+    modifier notLessThan(uint256 a, uint256 b) {
+        require(a >= b);
+        _;
+    }
+
+    modifier hasEnoughStocks(address addr, uint32 amount) {
+        require(stockholders[addr].stocksCount >= amount);
         _;
     }
 
@@ -37,6 +63,22 @@ contract Stocks {
 
     function withdraw(address payable targetAddress, uint256 amount) public isDirector notLargerThanBalance(amount) {
         targetAddress.transfer(amount);
+    }
+
+    function sellOrder(uint256 sellPrice, uint32 sellCount) public hasEnoughStocks(msg.sender, sellCount) {
+        stockholders[msg.sender].sellPrice = sellPrice;
+        stockholders[msg.sender].sellCount = sellCount;
+    }
+
+    function buyStocks(address payable sellerStockholder, uint32 buyCount) public payable 
+                                                                                hasEnoughStocks(sellerStockholder, buyCount) 
+                                                                                notLessThan(msg.value, stockholders[sellerStockholder].sellPrice * buyCount) {
+        uint256 transactionValue = stockholders[sellerStockholder].sellPrice * buyCount;
+        stockholders[sellerStockholder].sellCount -= buyCount;
+        stockholders[sellerStockholder].stocksCount -= buyCount;
+        stockholders[msg.sender].stocksCount += buyCount;
+        sellerStockholder.transfer(transactionValue);
+        payable(msg.sender).transfer(msg.value - transactionValue);
     }
 
 }
